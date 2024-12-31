f78439634b8b0ccb65b1f75eb2563f025879fe3a
كمبيولاب <@DESKTOP-V2DT3JS>
2024-12-31 12:38:35 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            int CarpetSmall = 25;
+            int CarpetLarge = 35;
+            double TaxRate = .06;
+            Console.WriteLine("Hello in carpet cleaning service");
+            Console.Write("Number of small carpets : ");
+            int NumSmall = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of Large carpets : ");
+            int NumLarge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Price per small carpet : {CarpetSmall:C}");
+            Console.WriteLine($"Price per Large carpet : {CarpetLarge:C}");
+            int PriceSmall = NumSmall * CarpetSmall;
+            int PriceLarge = NumLarge * CarpetLarge;
+            int Cost = PriceSmall + PriceLarge;
+            double Tax = Cost * TaxRate;
+            double Total = Tax + Cost;
+            Console.WriteLine($"Cost : {Cost:C}");
+            Console.WriteLine($"Tax : {Tax:C}");
+            Console.WriteLine($"Total estimate : {Total:C}");
+            Console.WriteLine("This estimate is valid for 30 days.");
+        }
+    }
+}
+
---
+++ TaskSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 24034.2457;
+            Console.WriteLine($"Int of num : {((int)num):D}");
+            Console.WriteLine($"Float of num : {((float)num):F3}");
+        }
+    }
+}
